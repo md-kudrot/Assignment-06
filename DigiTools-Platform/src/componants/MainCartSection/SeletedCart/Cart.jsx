5d2e50cf4aa4a33e5991d3b5e31698bfae5991ae
md-kudrot-592 <kk.mdkudrot@gmail.com>
2026-03-30 12:09:46 +0600

    feat: add Cart component and integrate it into SeletedCart for dynamic item rendering

---
+++ DigiTools-Platform/src/componants/MainCartSection/SeletedCart/Cart.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Cart = ({item}) => {
+    console.log(item)
+    return (
+        <div>
+            <div className="">
+                <div className="bg-[#f9fafc] px-8 py-4 flex gap-4 items-center justify-between">
+                    <div className="flex items-center gap-2">
+                        <p className="text-2xl">
+                            {item.icon}
+                        </p>
+                        <div className="">
+                            <h1 className='text-xl text-black font-semibold'>{item.name}</h1>
+                            <p className='text-[#627382]'>$ {item.price}</p>
+                        </div>
+                    </div>
+                    <div className="">
+                        <p className='text-red-500'>Remove</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
--- DigiTools-Platform/src/componants/MainCartSection/SeletedCart/SeletedCart.jsx
+++ DigiTools-Platform/src/componants/MainCartSection/SeletedCart/SeletedCart.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cart from './Cart';
 
 
 const SeletedCart = ({ buyCart }) => {
@@ -6,24 +7,12 @@ const SeletedCart = ({ buyCart }) => {
         <div>
             SeletedCart {buyCart.length}
             <div className="flex flex-col items-center justify-center">
-                <div className="card w-[70%] bg-base-100 card-md shadow-sm">
-                    <div className="card-body">
-                        <p>Your cart</p>
-                        <div className="bg-[#f9fafc] px-8 py-4 flex items-center justify-between">
-                            <div className="flex items-center gap-2">
-                                <h1>img</h1>
-                                <div className="">
-                                    <h1 className='text-xl text-black font-semibold'>Ai wiriting pro</h1>
-                                    <p className='text-[#627382]'>$29</p>
-                                </div>
-                            </div>
-                            <div className="">
-                                <p className='text-red-500'>Remove</p>
-                            </div>
-                        </div>
-                        <div className="justify-end card-actions">
-                            <button className="btn btn-primary">Buy Now</button>
-                        </div>
+                <div className="card w-[70%] bg-base-100 card-md shadow-sm p-4">
+                    <p>Your cart</p>
+                    <div className="flex flex-col gap-4">
+                        {
+                            buyCart.map(item => <Cart key={item.id} item={item}></Cart>)
+                        }
                     </div>
                 </div>
             </div>
